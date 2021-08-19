dc332976f8742c19166c53163fa5a859032c5b0d
Marvin Ren <marvin.ren@yale.edu>
2021-08-19 15:17:06 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,7 +72,12 @@ var fight = function(enemyName) {
      }
     }
 };
-
+// function to start a new game
+var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         // let player know what roud they are in, remember that arrays start at 0 so it needs to have 1 added to it
@@ -94,7 +99,31 @@ for (var i = 0; i < enemyNames.length; i++) {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
     }
-}
+  }
+  // after loop ends, we are either out of playerHealth or enemies to fight, so run the endGame function
+  endGame();
+  };
+  var endGame = function () {
+    window.alert("The game has now ended. Let's see how you did!");
+      if (playerHealth > 0) {
+          window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+      }
+      else {
+          window.alert("You've lost your robot in battle.");
+      }
+      //ask player if they'd like to play again
+      var playAgainConfirm = window.confirm("Whould you like to play again?")
+
+      if (playAgainConfirm) {
+        //restart the game
+        startGame();
+      }
+      else {
+          window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+      }
+};
+//start the game when the page loads
+startGame();
 //fight();
 
 // Game States
