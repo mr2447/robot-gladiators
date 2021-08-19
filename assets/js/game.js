4689ff401175bc9ccbc37438ee59269b74b67cf7
Marvin Ren <marvin.ren@yale.edu>
2021-08-19 02:16:29 -0400

    updates improvements

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,14 +11,11 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-
-
-
+// fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
-    // repeat and execute as long as the enemy-robot is alive 
-    while(enemyHealth > 0 && enemyHealth > 0) {
+  while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter Any Key to fight or 'SKIP' to skip.");
     console.log(promptFight);
     
     // if player choses to skip confirm and then stop the loop
@@ -32,25 +29,20 @@ var fight = function(enemyName) {
             //subtract money from playerMoney for skipping
             playerMoney = playerMoney - 10;
             window.alert(playerName + " has " + playerMoney + " coin(s) remaining.");
-            console.log (
-                "playerMoney", playerMoney);
+            console.log ("playerMoney", playerMoney);
             break;
         }
     }
-
-    //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
     //remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth=enemyHealth - playerAttack;
+    enemyHealth = enemyHealth - playerAttack;
     window.alert(playerName + " attacked " + enemyName + ".");
     console.log(
-        playerName + " attacked " + enemyName + "."
+        playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining."
     )
-    // Log a resulting messagge to the console so we know that it worked.
-   
   
     // check enemy's health
     if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+      window.alert(enemyName + " has died!");
 
         // award player Money for winning
         playerMoney = playerMoney + 20;
@@ -63,30 +55,28 @@ var fight = function(enemyName) {
         window.alert(enemyName + " still has " + enemyHealth + " health left."); 
     } 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-     playerHealth=playerHealth - enemyAttack;
+     playerHealth = playerHealth - enemyAttack;
      window.alert(enemyName + " attacked " + playerName + ".");
 
      console.log(
-         enemyName + " attacked " + playerName + "."
+         enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
      )
-     
-     //log a resulting message to the console so we know that it worked. 
      
      // Check player's health
      if (playerHealth <= 0) {
          window.alert(playerName + " has died!");
         // leave while () loop if player is dead
-         break;
+        break;
      } else {
          window.alert(playerName + " still has " + playerHealth + " health left.")
      }
     }
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
+for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         // let player know what roud they are in, remember that arrays start at 0 so it needs to have 1 added to it
-        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        window.alert ("Welcome to Robot Gladiators! Round " + (i + 1));
         
         // pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyName = enemyNames[i];
@@ -95,7 +85,7 @@ for(var i = 0; i < enemyNames.length; i++) {
         enemyHealth = 50;
         
         // use debugger to pause script from running and check what's going on at thtat moment in the code
-        debugger;
+        //debugger;
 
         // pass the pickedEnemyName variabe's value into the fight function, where it will assume the value of the enemyName parameter 
        fight(pickedEnemyName);
